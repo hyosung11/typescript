9a3ae0c86da70d81f9828cc4f25ca8a956f04772
HyoSung Bidol-Lee <hyosung11@me.com>
2019-12-30 16:53:43 -0500

    TypeScript Types

---
+++ typescript.js
@@ -0,0 +1,18 @@
+// Boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'brown';
+var favoriteQuote = "I don't feel old, I'm only " + age;
+// arrray
+var pets = ['cat', 'dog', 'pig'];
+// another way to declare arrays
+var pets2 = ['lion', 'dragon', 'gargoyle'];
+// object - lowercase
+var wizard = {
+    a: 'John'
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
---
+++ typescript.ts
@@ -0,0 +1,24 @@
+// Boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'brown';
+let favoriteQuote: string = `I don't feel old, I'm only ${age}`;
+
+// arrray
+let pets: string[] = ['cat', 'dog', 'pig'];
+
+// another way to declare arrays
+let pets2: Array<string> = ['lion', 'dragon', 'gargoyle'];
+
+// object - lowercase
+let wizard: object = {
+  a: 'John'
+}
+
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
